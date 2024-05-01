f9229788cc11b6af15977a85a3f13d8b9e464f24
Paulina Velasquez <41736073+paulinavelasquez@users.noreply.github.com>
2024-05-01 12:09:50 -0300

    Update yolo_converter.py

--- utils/yolo_converter.py
+++ utils/yolo_converter.py
@@ -162,7 +162,8 @@ class YOLOAnnotationConverter():
             return json.dumps(task).encode()
     
     def from_de(self, row):
-        if 'split' not in row:
+        if "split" not in row.metadata
+       # if 'split' not in row:
             print("Skipping datapoint due to missing 'split'")
             return
         annotation_data = row["annotation"]
